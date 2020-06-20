dc72c1b1ebc10e061f360b58706db5ba315c0135
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-20 18:48:17 +0530

    Debugging

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express=require('express');
 const bodyParser=require('body-parser');
 var app=express();
 app.use(bodyParser.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello");
+})
 app.get('/api',(req,res)=>{
     res.send({data:"Press Enter or click on search"});
-})
+});
 
-app.listen(5500,()=>{
-    console.log("Listening on port 5500")
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    const path=require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+const port=process.env.PORT||5500;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
 });
